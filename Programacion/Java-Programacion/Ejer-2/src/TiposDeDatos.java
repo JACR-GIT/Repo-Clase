a055824e96a4a1fc7775fb314ae0cb83379bd7c3
JACR-GIT <jcamposrueda@safareyes.es>
2025-01-15 11:43:17 +0100

    LocoWeen

---
+++ Programacion/Java-Programacion/Ejer-2/src/TiposDeDatos.java
@@ -0,0 +1,13 @@
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        int edad = 12;
+        double altura = 1.75;
+        char inicial = 'H';
+        boolean esEstudiante = true;
+
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Estado: " + esEstudiante);
+    }
+}
