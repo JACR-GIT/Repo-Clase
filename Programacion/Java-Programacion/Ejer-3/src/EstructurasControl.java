f7aac7aa689e099fb28896a5e0d0bc4776fdd6bd
JACR-GIT <jcamposrueda@safareyes.es>
2025-01-23 13:39:29 +0100

    lol

---
+++ Programacion/Java-Programacion/Ejer-3/src/EstructurasControl.java
@@ -0,0 +1,23 @@
+public class EstructurasControl {
+    public static void main(String[] args) {
+        int numero = 6;
+
+        //Condicional
+
+        if (numero >=1) {
+            System.out.println("El numero es positivo.");
+        }
+        else if (numero <=-1) {
+            System.out.println("El numero es negativo.");
+        }
+        else if (numero == 0) {
+            System.out.println("El numero es zero.");
+        }
+
+        //Bucle
+        for ( int i <= 10) {
+            int i++;
+            System.out.println(i);
+        }
+    }
+}
