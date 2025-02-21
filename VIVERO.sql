f3c9f42591e010e2644e17111a8ca882bb04e976
JACR-GIT <jcamposrueda@safareyes.es>
2025-02-21 12:56:30 +0100

    Actividades de .xml

--- VIVERO.sql
+++ VIVERO.sql
@@ -6,26 +6,89 @@ SELECT e.nombre,
     e.codigo_oficina,
     RPAD(UPPER(SUBSTR(e.nombre, 1, 2)),8,'?!45') AS "codigo_acceso"
 FROM empleado e
-WHERE e.puesto NOT LIKE '%Ventas%';
+WHERE e.puesto NOT LIKE '%Ventas%'
+Order by 1,2,3;
 
 -- 2
-SELECT 'Spain' AS pais,
-    COUNT(*) AS cantidad_de_clientes
+SELECT pais, COUNT(*) AS cantidad_de_clientes
 FROM cliente
-WHERE pais = 'Spain';
+WHERE pais = 'Spain'
+Group by pais;
 
 -- 3
 SELECT COUNT(*) AS "Cantidad de pedidos no entregados a tiempo"
 FROM pedido
 WHERE estado = 'Pendiente' OR (fecha_entrega IS NOT NULL AND fecha_entrega > fecha_esperada);
 
--- 4
-SELECT c.nombre_cliente AS "Nombre del Cliente",
-    e.puesto ,
-    c.nombre_contacto,
-    e.apellido1,
-    e.apellido2
-FROM cliente c
-JOIN empleado e ON c.codigo_empleado_rep_ventas = e.codigo_empleado
-WHERE e.puesto LIKE 'Representante Ventas%'
-order by e.apellido2;
+--4
+SELECT 
+    c.NOMBRE_CLIENTE, 
+    e.NOMBRE, 
+    e.APELLIDO1, 
+    e.APELLIDO2, 
+    e.PUESTO
+FROM C##VIVERO.CLIENTE c
+JOIN C##VIVERO.EMPLEADO e 
+    ON c.CODIGO_EMPLEADO_REP_VENTAS = e.CODIGO_EMPLEADO
+JOIN C##VIVERO.PAGO p 
+    ON c.CODIGO_CLIENTE = p.CODIGO_CLIENTE
+WHERE e.PUESTO LIKE '%Representante Ventas%'
+ORDER BY c.NOMBRE_CLIENTE;
+
+
+-- 5
+SELECT 
+    c.NOMBRE_CLIENTE, 
+    e.NOMBRE AS NOMBRE_REPRESENTANTE, 
+    e.APELLIDO1, 
+    e.APELLIDO2, 
+    e.PUESTO, 
+    o.CIUDAD AS CIUDAD_OFICINA
+FROM C##VIVERO.CLIENTE c
+JOIN C##VIVERO.EMPLEADO e 
+    ON c.CODIGO_EMPLEADO_REP_VENTAS = e.CODIGO_EMPLEADO
+JOIN C##VIVERO.OFICINA o 
+    ON e.CODIGO_OFICINA = o.CODIGO_OFICINA
+JOIN C##VIVERO.PAGO p 
+    ON c.CODIGO_CLIENTE = p.CODIGO_CLIENTE
+WHERE e.PUESTO LIKE '%Representante Ventas%'
+ORDER BY c.NOMBRE_CLIENTE;
+
+--6
+select o.ciudad, o.pais, o.region, o.codigo_postal, o.linea_direccion1, o.linea_direccion2
+from oficina o
+join empleado e on o.codigo_oficina = e.codigo_oficina
+join cliente c on c.codigo_empleado_rep_ventas = e.codigo_empleado
+where c.ciudad like 'Fuenlabrada';
+
+--7
+Select nombre_cliente
+from cliente natural join pedido
+where pedido.fecha_entrega>pedido.fecha_esperada
+order by 1;
+
+--8
+Select distinct p.gama
+from producto p natural join detalle_pedido d
+order by 1;
+
+--9
+SELECT c.nombre_cliente
+from cliente c left join pago p on c.codigo_cliente = p.codigo_cliente
+where p.codigo_cliente is null
+order by 1;
+
+--10
+SELECT c.nombre_cliente
+from cliente c left join producto p on c.codigo_producto = p.codigo_producto
+where p.codigo_cliente is null
+order by 1;
+
+--11
+select codigo_pedido, sum (cantidad)
+from detalle_pedido
+group by codigo_pedido
+order by 1;
+
+--12
+Select nombre_cliente, limite_cliente_credito || '€' "Cantidad Total"
