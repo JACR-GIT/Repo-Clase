5626dfe7963657807248a70a2b7c89c975d3a4f9
JACR-GIT <jcamposrueda@safareyes.es>
2025-02-24 14:23:16 +0100

    Dejame en paz ya porfavor

--- Programacion/Java-Programacion/Aseguradora/src/test/java/PruebaV1.java
+++ Programacion/Java-Programacion/Aseguradora/src/test/java/PruebaV1.java
@@ -3,13 +3,13 @@ import modelos.*;
 
 import java.time.LocalDate;
 
-public class pruebaV1 {
+public class PruebaV1 {
     public static void main(String[] args) {
         Direccion direccion = new Direccion(1, TipoVia.CALLE, "Gran Via", 10, "2B", "28013", "Madrid", new Provincia("1","Madrid"));
         Persona persona = new Persona(1, "Juan", "Perez", "Gomez", "12345678A", LocalDate.of(1990, 1, 1), direccion);
         Conductor conductor = new Conductor(2, "Ana", "Lopez", "Martinez", "87654321B", LocalDate.of(1985, 5, 5), direccion, LocalDate.of(2005, 5, 5), 12, 5);
         Vehiculo vehiculo = new Vehiculo(1, "Toyota", "Corolla", "1234ABC", LocalDate.of(2015, 6, 15), "Rojo", persona);
-        Coche coche = new Coche(2, "Ford", "Focus", "5678DEF", LocalDate.of(2018, 3, 20), "Azul", persona, 5, Coche.TipoCombustible.GASOLINA, Coche.Traccion.DELANTERA, false);
+        Coche coche = new Coche(2, "Ford", "Focus", "5678DEF", LocalDate.of(2018, 3, 20), "Azul", persona, 5, Coche.TipoCombusible.GASOLINA, Coche.TipoTraccion.DELANTERA, false);
         Moto moto = new Moto(3, "Yamaha", "R1", "9101GHI", LocalDate.of(2020, 8, 25), "Negro", persona, 1000, false);
 
         System.out.println(persona);
