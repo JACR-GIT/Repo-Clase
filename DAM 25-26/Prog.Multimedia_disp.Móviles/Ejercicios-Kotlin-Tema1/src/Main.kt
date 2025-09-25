1f526fdc08c05b826385b65eba93ce6c81446316
JACR-GIT <jcamposrueda@safareyes.es>
2025-09-25 13:18:32 +0200

    Clase

---
+++ DAM 25-26/Prog.Multimedia_disp.Móviles/Ejercicios-Kotlin-Tema1/src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var a = 2
+    var b = 3
+    val c = 6
+}
