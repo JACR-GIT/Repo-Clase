9b3499b4d84c4734a53ffd5d9feccad5d2292a26
JACR-GIT <jcamposrueda@safareyes.es>
2025-10-01 09:28:26 +0200

    01/ clase

---
+++ DAM 25-26/Acceso_a_datos/App_Prueba/src/main/java/com/safa/app_prueba/modelos/Fresa.java
@@ -0,0 +1,22 @@
+package com.safa.app_prueba.modelos;
+
+import lombok.*;
+
+@Getter
+
+public class Fresa {
+
+    private Integer id;
+
+    private String codigo;
+
+    private String nombre;
+
+    private Integer tipo;
+
+    private Integer temporada;
+
+    private Boolean rendimiento;
+
+
+}
