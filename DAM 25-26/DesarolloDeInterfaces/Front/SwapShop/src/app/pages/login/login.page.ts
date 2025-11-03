5ef1ae8948d714e01926443cc33c5ab1d8802251
JACR-GIT <jcamposrueda@safareyes.es>
2025-11-03 11:53:18 +0100

    A piñon

---
+++ DAM 25-26/DesarolloDeInterfaces/Front/SwapShop/src/app/pages/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {FooterComponent} from "../../components/footer/footer.component";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, FooterComponent]
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
