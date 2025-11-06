db69eb7972f965d1e6754998aecbd9fce981efbf
JACR-GIT <jcamposrueda@safareyes.es>
2025-11-06 22:26:09 +0100

    Cambios de color y colocacion del formulario-login
    Hay un error que hace que no se recargue la pagina y no es funcional.

--- DAM 25-26/DesarolloDeInterfaces/Front/SwapShop/src/app/formulario-login/formulario-login.component.ts
+++ DAM 25-26/DesarolloDeInterfaces/Front/SwapShop/src/app/formulario-login/formulario-login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {IonButton, IonCheckbox, IonCol, IonGrid, IonInput, IonItem, IonLabel, IonRow} from "@ionic/angular/standalone";
 
 @Component({
@@ -14,13 +15,28 @@ import {IonButton, IonCheckbox, IonCol, IonGrid, IonInput, IonItem, IonLabel, Io
     IonRow,
     IonCol,
     IonCheckbox,
-    IonButton
+    IonButton,
+    FormsModule
   ]
 })
-export class FormularioLoginComponent  implements OnInit {
+export class FormularioLoginComponent {
+
+  // Propiedades enlazadas a los inputs
+  email: string = '';
+  password: string = '';
+  remember: boolean = false;
 
   constructor() { }
 
-  ngOnInit() {}
+  // Método simple para manejar el envío de login
+  protected credentials: any;
+  login() {
+    // Aquí podrías validar y llamar a un servicio de autenticación
+    console.log('Login intentado', { email: this.email, password: this.password, remember: this.remember });
+    // Ejemplo: mostrar un alert o navegar
+  }
 
+  protected goToRegister() {
+
+  }
 }
