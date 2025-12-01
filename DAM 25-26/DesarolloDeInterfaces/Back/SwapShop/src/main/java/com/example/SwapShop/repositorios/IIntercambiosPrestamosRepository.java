7be5da0116feefd45c785e2e11a350a0c91ac17e
JACR-GIT <jcamposrueda@safareyes.es>
2025-12-01 14:18:37 +0100

    .

--- DAM 25-26/DesarolloDeInterfaces/Back/SwapShop/src/main/java/com/example/SwapShop/repositorios/IIntercambiosPrestamosRepository.java
+++ DAM 25-26/DesarolloDeInterfaces/Back/SwapShop/src/main/java/com/example/SwapShop/repositorios/IIntercambiosPrestamosRepository.java
@@ -1,9 +1,27 @@
 package com.example.SwapShop.repositorios;
 
 import com.example.SwapShop.modelos.IntercambiosPrestamos;
+import com.example.SwapShop.modelos.Prendas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IIntercambiosPrestamosRepository extends JpaRepository<IntercambiosPrestamos, Integer> {
+
+    @Query(value = "SELECT \n" +
+            "p.id AS id_prenda,\n" +
+            "p.nombre_prenda,\n" +
+            "p.descripcion,\n" +
+            "p.talla,\n" +
+            "p.categoria,\n" +
+            "p.condicion,\n" +
+            "COUNT(i.id) AS total_intercambios\n" +
+            "FROM prendas p\n" +
+            "INNER JOIN intercambios i ON i.id_prenda = p.id\n" +
+            "GROUP BY p.id\n" +
+            "ORDER BY total_intercambios DESC\n" +
+            "LIMIT 5;\n", nativeQuery = true)
+    List<Prendas> buscarTop5Intercambio();
 }
