a3bbe7a1b7a3f855e7a6d64467536fd1b68bd370
JACR-GIT <jcamposrueda@safareyes.es>
2026-02-02 20:26:24 +0100

    .

--- DAM25-26/DesarolloDeInterfaces/Back/SwapShop/src/main/java/com/example/SwapShop/config/CorsConfig.java
+++ DAM25-26/DesarolloDeInterfaces/Back/SwapShop/src/main/java/com/example/SwapShop/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
                 .allowedOrigins(
-                        "http://swapshop-f96d.onrender.com/",
+                        "https://repo-clase.onrender.com/",
                         "https://localhost/",
                         "http://localhost/",
                         "capacitor://localhost",
